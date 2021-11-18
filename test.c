337ed2b9f7940f00f0d2724df94910fdcc931093
ssanjan <syed_wahid_ferdous_sanjan@sfu.ca>
2021-11-18 13:41:39 +0000

    Upload New File

---
+++ test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main(void)
+{
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC,&start);
+
+    //Test for built in malloc and free
+    int **a = malloc(10000*sizeof(*a));
+    for(int i = 0; i < 10000;i++){
+        a[i] = malloc(20000*sizeof(a));
+        for(int j = 0;j<20000;j++){
+            a[i][j] = j;
+        }
+    }
+
+    for(int i = 0;i<10000;i++){
+        free(a[i]);
+    }
+    free(a);
+
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+  
+    // Program time taken to complete
+    double time;
+    time = (end.tv_sec - start.tv_sec) * 1e9; //Second to nanosecond
+    time = (time + (end.tv_nsec - start.tv_nsec)) * 1e-9; //nano second to second with high precision
+    printf("Time taken by program is : %f sec \n", time);
+
+    return 0;
+}
