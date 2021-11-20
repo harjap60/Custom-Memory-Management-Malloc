413ce0ed8ade35883db356a9eb64d9969ac9a382
harjapdhaliwal <hda36@sfu.ca>
2021-11-19 20:03:17 -0800

    final submission

--- my_malloc.h
+++ my_malloc.h
@@ -1,6 +1,4 @@
-
-#include <stdlib.h>
-#include <stddef.h>
+//Functions for memory management
 
 void* my_malloc(size_t size);
 
@@ -8,15 +6,3 @@ void my_free(void *ptr);
 
 void mem_init();
 
-
-int mem_holes();
-int mem_allocated();
-int mem_free();
-int mem_total();
-int mem_largest_free();
-int mem_small_free(int size);
-char mem_is_alloc(void *ptr);
-void* mem_pool();
-void print_memory();
-void print_memory_status();
-void try_mymem(int argc, char **argv);
